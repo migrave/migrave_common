c334d072a3de6189aacce1dddafdef7426e35406
Mohammad Wasil <mwasil@outlook.co.id>
2021-09-17 13:46:18 +0200

    Changed migrave_common_utils -> migrave_common, migrave_common_ros_utils -> migrave_common_ros

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['migrave_common_ros_utils',
-              'migrave_common_utils'],
-    package_dir={'migrave_common_ros_utils': 'ros/src/migrave_common_ros_utils',
-                 'migrave_common_utils': 'common/src/migrave_common_utils'}
+    packages=['migrave_common_ros',
+              'migrave_common'],
+    package_dir={'migrave_common_ros': 'ros/src/migrave_common_ros',
+                 'migrave_common': 'common/src/migrave_common'}
 )
 
 setup(**d)
