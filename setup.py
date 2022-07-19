92e849488f8de913c60c9214f6f20f86585f4c30
Intelwi <mlk04@wp.pl>
2022-07-19 14:43:22 +0200

    Added ftsm components

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['migrave_common_ros',
+    packages=['migrave_common_test',
+              'migrave_common_ros',
               'migrave_common'],
-    package_dir={'migrave_common_ros': 'ros/src/migrave_common_ros',
+    package_dir={'migrave_common_test': 'common/src/migrave_common_test',
+                 'migrave_common_ros': 'ros/src/migrave_common_ros',
                  'migrave_common': 'common/src/migrave_common'}
 )
 
